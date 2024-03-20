8d9768581fa54737dfa073a5c747ece71a7dfd83
pjwstk28537 <s28537@pjwstk.edu.pl>
2024-03-20 21:43:29 +0100

    Task 2 created function e_sqaures for list of squares

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-squares = [x**2 for x in range(1, 11)]
-print(squares)
+def e_squares(begin, end):
+    squares = [x**2 for x in range(begin, end+1)]
+    return squares
+
+if __name__ == "__main__":
+    begin_num = int(input("Enter begin num: "))
+    end_num = int(input("Enter ending num: "))
+
+    output = e_squares(begin_num, end_num)
+
+    print(output)
