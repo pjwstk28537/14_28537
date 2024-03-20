198f5bb2dba6992f86de867542847b4fc6aeaffa
pjwstk28537 <s28537@pjwstk.edu.pl>
2024-03-20 21:49:21 +0100

    Task 3 added SquareGenerator class

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-def e_squares(begin, end):
-    squares = [x**2 for x in range(begin, end+1)]
-    return squares
+class SquareGenerator:
+    def e_squares(self, begin, end):
+        squares = [x**2 for x in range(begin, end+1)]
+        return squares
+
 
 if __name__ == "__main__":
+    square_generator = SquareGenerator()
     begin_num = int(input("Enter begin num: "))
     end_num = int(input("Enter ending num: "))
 
-    output = e_squares(begin_num, end_num)
+    output = square_generator.e_squares(begin_num, end_num)
 
     print(output)
+
