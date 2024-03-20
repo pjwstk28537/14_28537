5410295456e548861c39652c56882b67a071daff
pjwstk28537 <s28537@pjwstk.edu.pl>
2024-03-21 00:01:06 +0100

    Task 10 convert SquareGenerator class to be abstract

--- main.py
+++ main.py
@@ -6,7 +6,5 @@ if __name__ == "__main__":
     begin_num = int(input("Enter begin num: "))
     end_num = int(input("Enter ending num: "))
 
-    output = SquareGenerator.e_squares(begin_num, end_num)
     output_two = CubicGenerator.e_squares(begin_num, end_num)
-    print(output)
     print(output_two)
--- square_generator/__init__.py
+++ square_generator/__init__.py
@@ -1,12 +1,7 @@
-import math
+from abc import ABC, abstractmethod
 
 
-class SquareGenerator:
-    @staticmethod
-    def e_squares(begin, end):
-        if end < begin:
-            begin, end = end, begin
-            print("begin num was larger than ending num, so they were switched")
-            # optionally = raise ValueError("begin num is larger tan ending num!")
-        squares = [(x, math.sqrt(x)) for x in range(begin, end + 1)]
-        return squares
+class SquareGenerator(ABC):
+    @abstractmethod
+    def e_squares(self, begin, end):
+        pass
