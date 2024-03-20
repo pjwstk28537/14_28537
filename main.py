a158e94de60caae3d0c216ec3bf5f20ae97e3ae4
pjwstk28537 <s28537@pjwstk.edu.pl>
2024-03-20 22:01:16 +0100

    Task 4 imported and implemented math library (+ made method static to not need initialization in if statement)

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+import math
+
+
 class SquareGenerator:
-    def e_squares(self, begin, end):
-        squares = [x**2 for x in range(begin, end+1)]
+    @staticmethod
+    def e_squares(begin, end):
+        squares = [(x,math.sqrt(x)) for x in range(begin, end + 1)]
         return squares
 
 
 if __name__ == "__main__":
-    square_generator = SquareGenerator()
     begin_num = int(input("Enter begin num: "))
     end_num = int(input("Enter ending num: "))
 
-    output = square_generator.e_squares(begin_num, end_num)
-
+    output = SquareGenerator.e_squares(begin_num, end_num)
     print(output)
-
