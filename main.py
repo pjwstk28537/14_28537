f413d6c68898d9799852ef23e68208729f7425a5
pjwstk28537 <s28537@pjwstk.edu.pl>
2024-03-20 22:59:40 +0100

    Task 5 handle exceptions when begin larger than end

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import math
 class SquareGenerator:
     @staticmethod
     def e_squares(begin, end):
-        squares = [(x,math.sqrt(x)) for x in range(begin, end + 1)]
+        if end < begin:
+            begin, end = end, begin
+            print("begin num was larger than ending num, so they were switched")
+            # optionally = raise ValueError("begin num is larger tan ending num!")
+        squares = [(x, math.sqrt(x)) for x in range(begin, end + 1)]
         return squares
 
 
